0a923ca60888bf9d87ddbae1c8d9597675ed9229
yukimasa ikeda <yyykms123@gmail.com>
2020-11-07 14:50:48 +0900

    done chapter-4

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -3,17 +3,26 @@
 namespace App\Http\Controllers;
 
 use App\Models\Folder;
+use App\Models\Task;
 use Illuminate\Http\Request;
 
 class TaskController extends Controller
 {
     public function index(int $id)
     {
+        // 全てのフォルダを取得
         $folders = Folder::all();
+
+        // 選ばれたフォルダを取得
+        $current_folder = Folder::find($id);
+
+        // 選ばれたフォルダに紐づくタスクを取得
+        $tasks = $current_folder->tasks()->get();
 
         return view('tasks/index', [
             'folders' => $folders,
             'current_folder_id' => $id,
+            'tasks' => $tasks,
         ]);
     }
 }
--- app/Models/Folder.php
+++ app/Models/Folder.php
@@ -8,4 +8,18 @@ use Illuminate\Database\Eloquent\Model;
 class Folder extends Model
 {
     use HasFactory;
+
+    public function tasks()
+    {
+        /**
+         * return $this->hasMany('App\Models\Task, 'folder_id', 'id');
+         * 第一引数：関連数モデル名
+         * 第二引数：関連するテーブルが持つ外部キーカラム名
+         * 第三引数：モデルにhasManyが定義されている側のテーブルが持つ外部キーに紐づけられたカラム名
+         *
+         * 二引数が「テーブル名単数形_id」で第三引数が「id」であれば省略が可能
+         */
+
+        return $this->hasMany('App\Models\Task');
+    }
 }
