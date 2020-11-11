6ed6732ddefd202853b685eaa64c49ba10dbe3a1
yukimasa ikeda <yyykms123@gmail.com>
2020-11-11 22:42:07 +0900

    done chapter-4

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -3,17 +3,24 @@
 namespace App\Http\Controllers;
 
 use App\Models\Folder;
+use App\Models\Task;
 use Illuminate\Http\Request;
 
 class TaskController extends Controller
 {
     public function index(int $id)
     {
+        // 全てのフォルダを取得する
         $folders = Folder::all();
+        // 選ばれたフォルダを取得する
+        $current_folder = Folder::find($id);
+        // 選ばれたフォルダに紐づくタスクを取得する
+        $tasks = $current_folder->tasks()->get();
 
         return view('tasks/index', [
             'folders' => $folders,
-            'current_folder_id' => $id,
+            'current_folder_id' => $current_folder->id,
+            'tasks' => $tasks,
         ]);
     }
 }
